782f0a4cc615c966d73dcceccf2d4ccf19df0a80
yanshaoyao-boop <yanshaoyao@gmail.com>
2026-02-04 09:41:02 +0800

    chore: setup github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  // 如果部署在 https://<user>.github.io/<repo>/，需要开启 basePath
+  basePath: '/drayage-optimizer',
 };
 
 export default nextConfig;
